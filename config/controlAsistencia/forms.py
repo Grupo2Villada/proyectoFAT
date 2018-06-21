65f84774e6b592027d2a189c7a36a71102e3a3be
julian <juli.luna1999@gmail.com>
2018-06-21 12:24:01 -0300

    max/min values for forms

--- config/controlAsistencia/forms.py
+++ config/controlAsistencia/forms.py
@@ -19,9 +19,9 @@ class PreceptorUpdateForm(forms.Form):
 class StudentForm(forms.Form):
 	first_name = forms.CharField(max_length=12)
 	last_name = forms.CharField(max_length=25)
-	dni = forms.IntegerField()
-	student_tag = forms.IntegerField()
-	list_number = forms.IntegerField()
+	dni = forms.IntegerField(max_value=99999999, min_value=1)
+	student_tag = forms.IntegerField(max_value=99999, min_value=0)
+	list_number = forms.IntegerField(max_value=45, min_value=1)
 	birthday = forms.DateField(widget = forms.SelectDateWidget(years=range(1900,2019)))
 	address = forms.CharField(max_length=50)
 	neighbourhood = forms.CharField(max_length=50)
